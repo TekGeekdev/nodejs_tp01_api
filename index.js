21564ef93cccbaa330f21007b93cf24838f33b5b
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-05-04 00:00:21 -0400

    add scrapping route index.js

--- index.js
+++ index.js
@@ -7,10 +7,33 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/ticker=:id', (req,res)=>{
-    const ticker = req.params.id
-    const url= `https://api.petfinder.com/v2/animals`
-    
+app.get('/scrapping/movie/:id', (req,res)=>{
+    const movie = req.params.id
+    const url= `https://api.themoviedb.org/3/movie/${movie}`
+      
+    request.get({
+        url: url,
+        headers: {
+            accept: 'application/json',
+          Authorization: `Bearer ${config.API_KEY}`
+        }
+    }, (err, response, data) => {
+        if (err){
+            console.log("Erreur:", err);
+        }else if(response.statusCode !== 200){
+            console.log('Status:', response.statusCode);
+        }else{
+            fs.writeFile(`data/${movie}.json`, data, (err) => {
+                if (err) {
+                    return res.status(500).send('Erreur lors de la sauvegarde du fichier.');
+                }
+                res.send('Fichier sauvegardé !');
+            });
+        }
+})
 })
 
-console.log(config.PORT);
+const PORT = config.PORT
+app.listen(PORT, () =>{
+    console.log(`server running on port ${PORT}`)
+})
